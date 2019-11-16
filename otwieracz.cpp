38c59c5c4f12fb57976e70b98595fb0e7bae6892
ciszekxd <44375349+ciszekxd@users.noreply.github.com>
2019-11-17 00:18:17 +0100

    dziala

--- otwieracz.cpp
+++ otwieracz.cpp
@@ -1,7 +1,7 @@
 
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <set>
 
 using namespace std;
 class Otwieracz {
@@ -105,8 +105,7 @@ protected:
 };
 class Solver{
 public:
-    vector<int > done_exercises;
-    vector<int*> validProcesses;
+    set<int*> validProcesses;
     int time_dim;
     int machine_dim;
     int** machinesT;
@@ -118,8 +117,30 @@ public:
         timesT = times;
         machinesT = machines;
     }
+    int** setProcessor(int** procT, int procAmount) {
 
+        int* arm;
+        for (int i = 0; i < procAmount; ++i) {
+            set<int*>::iterator iterator = validProcesses.begin();
+            while (iterator != validProcesses.end()) {
+                if (procT[i][0]==-1){
+                    arm = *iterator;
+                    if(i == machinesT[arm[0]][arm[1]]){
+                        procT[i][0] = timesT[arm[0]][arm[1]];
+                        procT[i][1] = arm[0];
+                        procT[i][2] = arm[1];
+                        validProcesses.erase(iterator);
+                    }
+                }
+            iterator++;
+            }
+        }
+        return procT;
+    }
 
+    void upadteTimesT(int** newTT){
+        timesT = newTT;
+    }
     int* find_min_time(int col){
         int ans[3];
         int* answ = ans;
@@ -136,24 +157,33 @@ public:
         ans[2] = x;
         return answ;
     }
-    void prepareValidProcesses(){
-        int obs1=0,obs2=0;
-        int* x;
-        for (int i = 0; i < time_dim; ++i) {
-            for (int j = 0; j < time_dim; ++j) {
-                if (timesT[i][j] == -1){
-                    obs1 = j;
-                    obs2 = i;
-                    break;
-                }
-            }
-            x = new int[2];
-            x[0] = timesT[obs2][obs1];
-            x[1] = machinesT[obs2][obs1];
+    int hardcheck(int* exe){
+        set<int*>::iterator iterator = validProcesses.begin();
+        int* x = *iterator;
+        while(iterator != validProcesses.end()){
 
-            validProcesses.push_back(x);
+            if((x[0]==exe[0])&&(x[1]==exe[1])) return 0;
+            iterator++;
         }
+        return 1;
 
+    }
+    void prepareValidProcesses(){
+
+        int j=0;
+
+        for (int i = 0; i < time_dim; ++i) {
+            if (timesT[i][j] == -1) {
+                j++;
+                i--;
+                continue;
+            }
+            int* x = new int[2];;
+            x[0] = i;
+            x[1] = j;
+            if (!hardcheck(x)) continue;
+            validProcesses.insert(x);
+        }
     }
 
 };
@@ -161,27 +191,90 @@ class Processor{
 public:
     int** procek;
     int coreAmount;
-    Processor(int core){
-        procek = new int*[core];
-        for (int i = 0; i < coreAmount; ++i) {
-            procek[i] = nullptr;
+    int T;
+    int** finishLineup;
+
+
+    Processor(int core,int execs){
+        procek = maketab(3, core);
+        for (int i = 0; i < core; ++i) {
+            procek[i][0] = -1;
         }
         coreAmount = core;
+        T = 0;
+        finishLineup = maketab(execs,execs);
     }
-    void freeProcessor(){
+    void procekUpdate(int** uproc){
+        procek = uproc;
+    }
+    void makeProcessorStep(){
+        int min = 2147483647;
         for (int i = 0; i < coreAmount; ++i) {
-            if(procek[i][1] == 0) procek[i] = nullptr;
+            if ((procek[i][0] < min) && (procek[i][0] != -1)){
+                min = procek[i][0];
+            }
         }
+        for (int j = 0; j < coreAmount; ++j) {
+            if (procek[j][0]==-1) continue;
+            procek[j][0] -= min;
+        }
+        T += min;
+    }
+    int** freeProcessor(int** timesT){
+        for (int i = 0; i < coreAmount; ++i) {
+            if (procek[i][0]==0) {
+                procek[i][0] = -1;
+                timesT[procek[i][1]][procek[i][2]] = -1;
+                finishLineup[procek[i][1]][procek[i][2]] = T;
+            }
+        }
+        return timesT;
+    }
+    int** maketab(int a, int b) {
+        int **x = new int *[b];
+        for (int i = 0; i < b; ++i) {
+            x[i] = new int[a];
+        }
+        return x;
     }
 };
+void poka(int**T,int dim) {
+    for (int i = 0; i < dim; ++i) {
+        for (int j = 0; j < dim; ++j) {
+            cout << T[i][j];
+            cout << "  ";
+        }
+        cout << endl;
+    }
+}
+int checkend(int** matrix,int exe){
+    for (int i = 0; i < exe; ++i) {
+        if(matrix[i][exe-1] != -1) return 1;
+    }
+    return 0;
+}
 int main(){
-    string path = "/home/ciszek/OK/tai01.txt";
-    Otwieracz bulgaria(path, "t");
-    cout << bulgaria.zadania << endl;
-    cout << bulgaria.machines << endl;
-    cout << bulgaria.timesTable[3][6] << endl;
-    cout << bulgaria.machinesTable[3][6] << endl;
+    string path = "/home/ciszek/OK/testcase.txt";
+    Otwieracz bulgaria(path, "b");
+
     Solver case1(bulgaria.zadania, bulgaria.machines, bulgaria.timesTable, bulgaria.machinesTable );
 
+    Processor intel(bulgaria.machines, bulgaria.zadania);
+
+    
+    //#######pętla###############
+    int obs=1;
+    while(obs) {
+        case1.prepareValidProcesses();
+        //problem
+        intel.procekUpdate(case1.setProcessor(intel.procek, intel.coreAmount));
+        // idea części dalszej
+        intel.makeProcessorStep();
+        case1.upadteTimesT(intel.freeProcessor(case1.timesT));
+        obs = checkend(case1.timesT,bulgaria.zadania);
+        //###########################
+    }
+    //rezultatem ma być intel.finishlineup
+    poka(intel.finishLineup,intel.coreAmount);
     return 0;
 }
